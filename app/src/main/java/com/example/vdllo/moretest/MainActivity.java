a7e658f554d88def89cd98763fcf7a192a1925a4
locas521 <locas521@outlook.com>
2016-03-29 10:07:49 +0800

    HNX完成跳转

---
+++ app/src/main/java/com/example/vdllo/moretest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vdllo.moretest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //HNX 完成跳转
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
